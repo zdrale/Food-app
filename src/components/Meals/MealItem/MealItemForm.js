b012bb140aaff9377e926006bd2996d1d999fad3
zdrale <nikolazd22@outlook.com>
2022-06-02 12:00:56 +0200

    Added logic for adding items to cart

--- src/components/Meals/MealItem/MealItemForm.js
+++ src/components/Meals/MealItem/MealItemForm.js
@@ -1,18 +1,44 @@
-import classes from "./MealItemForm.module.css"
+import { useRef, useState } from "react";
+import classes from "./MealItemForm.module.css";
 import Input from "../../UI/Input";
-const MealItemForm = props => {
-    return <form className={classes.form}>
-        <Input label="Amount" input={{
-            id:'amount_' + props.id,
-            type:'number',
-            min:'1',
-            max:'5',
-            step:'1',
-            defaultValue:'1'
-        }} />
-        <button> + Add</button>
-    </form>
-}
 
+const MealItemForm = (props) => {
+  const [amountIsValid, setAmountIsValid] = useState(true);
+  const amountInputRef = useRef();
+  const submitHandler = (event) => {
+    event.preventDefault();
+    const enteredAmount = amountInputRef.current.value;
+    const enteredAmountNumber = +enteredAmount;
+
+    if (
+      enteredAmount.trim().length === 0 ||
+      enteredAmountNumber < 1 ||
+      enteredAmountNumber > 5
+    ) {
+      setAmountIsValid(false);
+      return;
+    }
+
+    props.onAddToCart(enteredAmountNumber);
+  };
+  return (
+    <form className={classes.form}>
+      <Input
+        ref={amountInputRef}
+        label="Amount"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button onClick={submitHandler}> + Add</button>
+      {!amountIsValid && <p>Please enter valid amount (1-5)</p>}
+    </form>
+  );
+};
 
 export default MealItemForm;
